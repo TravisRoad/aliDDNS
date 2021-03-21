a3541d1efe20b8d96dba8d25257f3dfc470cde9a
Travis <lxy_montage@163.com>
2021-03-21 18:18:59 +0800

    init

---
+++ src/main/java/com/lxythan2lxy/alidns/DDNS.java
@@ -0,0 +1,193 @@
+package com.lxythan2lxy.alidns;
+
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.alidns.model.v20150109.DescribeSubDomainRecordsRequest;
+import com.aliyuncs.alidns.model.v20150109.DescribeSubDomainRecordsResponse;
+import com.aliyuncs.alidns.model.v20150109.UpdateDomainRecordRequest;
+import com.aliyuncs.alidns.model.v20150109.UpdateDomainRecordResponse;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.profile.DefaultProfile;
+import com.google.gson.Gson;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DDNS {
+    /**
+     * 获取主域名的所有解析记录列表
+     */
+    private DescribeSubDomainRecordsResponse describeSubDomainRecords(DescribeSubDomainRecordsRequest request,
+            IAcsClient client) {
+        try {
+            // 调用SDK发送请求
+            return client.getAcsResponse(request);
+        } catch (ClientException e) {
+            e.printStackTrace();
+            // 发生调用错误，抛出运行时异常
+            throw new RuntimeException();
+        }
+    }
+
+    /**
+     * 获取当前主机公网IP
+     */
+    private String getCurrenHostIP() {
+        // 这里使用jsonip.com第三方接口获取本地IP
+        String jsonip = "https://jsonip.com";
+        // 接口返回结果
+        String result = "";
+        BufferedReader in = null;
+        try {
+            // 使用HttpURLConnection网络请求第三方接口
+            URL url = new URL(jsonip);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+            in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 使用finally块来关闭输入流
+        finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        // 正则表达式，提取xxx.xxx.xxx.xxx，将IP地址从接口返回结果中提取出来
+        String rexp = "(\\d{1,3}\\.){3}\\d{1,3}";
+        Pattern pat = Pattern.compile(rexp);
+        Matcher mat = pat.matcher(result);
+        String res = "";
+        while (mat.find()) {
+            res = mat.group();
+            break;
+        }
+        return res;
+    }
+
+    /**
+     * 修改解析记录
+     */
+    private UpdateDomainRecordResponse updateDomainRecord(UpdateDomainRecordRequest request, IAcsClient client) {
+        try {
+            // 调用SDK发送请求
+            return client.getAcsResponse(request);
+        } catch (ClientException e) {
+            e.printStackTrace();
+            // 发生调用错误，抛出运行时异常
+            throw new RuntimeException();
+        }
+    }
+
+    private static void log_print(String functionName, Object result) {
+        Gson gson = new Gson();
+        System.out.println("-------------------------------" + functionName + "-------------------------------");
+        System.out.println(gson.toJson(result));
+    }
+
+    private static InetAddress getLocalHostExactAddress() {
+        try {
+            InetAddress candidateAddress = null;
+
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface iface = networkInterfaces.nextElement();
+                if (!iface.getDisplayName().equals("Intel(R) Wi-Fi 6 AX200 160MHz")) {
+                    continue;
+                }
+                // 该网卡接口下的ip会有多个，也需要一个个的遍历，找到自己所需要的
+                for (Enumeration<InetAddress> inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements();) {
+                    InetAddress inetAddr = inetAddrs.nextElement();
+                    // 排除loopback回环类型地址（不管是IPv4还是IPv6 只要是回环地址都会返回true）
+                    if (!inetAddr.isLoopbackAddress()) {
+                        if (inetAddr.isSiteLocalAddress()) {
+                            // 如果是site-local地址，就是它了 就是我们要找的
+                            // ~~~~~~~~~~~~~绝大部分情况下都会在此处返回你的ip地址值~~~~~~~~~~~~~
+                            return inetAddr;
+                        }
+
+                        // 若不是site-local地址 那就记录下该地址当作候选
+                        if (candidateAddress == null) {
+                            candidateAddress = inetAddr;
+                        }
+
+                    }
+                }
+            }
+
+            // 如果出去loopback回环地之外无其它地址了，那就回退到原始方案吧
+            return candidateAddress == null ? InetAddress.getLocalHost() : candidateAddress;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String get_LAN_IP() {
+        String rtn = " ";
+        rtn = getLocalHostExactAddress().getHostAddress();
+        System.out.println("本机的IP = " + rtn);
+        return rtn;
+    }
+
+    public static void main(String[] args) {
+        // 设置鉴权参数，初始化客户端
+        DefaultProfile profile = DefaultProfile.getProfile("cn-beijing", // 地域ID
+                "ak", // 您的AccessKey ID
+                "as");// 您的AccessKey Secret
+        IAcsClient client = new DefaultAcsClient(profile);
+        DDNS ddns = new DDNS();
+        // 查询指定二级域名的最新解析记录
+        DescribeSubDomainRecordsRequest describeSubDomainRecordsRequest = new DescribeSubDomainRecordsRequest();
+        describeSubDomainRecordsRequest.setSubDomain("example.com");
+        DescribeSubDomainRecordsResponse describeSubDomainRecordsResponse = ddns
+                .describeSubDomainRecords(describeSubDomainRecordsRequest, client);
+        log_print("describeSubDomainRecords", describeSubDomainRecordsResponse);
+        List<DescribeSubDomainRecordsResponse.Record> domainRecords = describeSubDomainRecordsResponse
+                .getDomainRecords();
+        // 最新的一条解析记录
+        if (domainRecords.size() != 0) {
+            DescribeSubDomainRecordsResponse.Record record = domainRecords.get(0);
+            // 记录ID
+            String recordId = record.getRecordId();
+            // 记录值
+            String recordsValue = record.getValue();
+            // 当前主机公网IP
+            String currentHostIP = ddns.get_LAN_IP();
+            System.out.println(
+                    "-------------------------------当前局域网IP为：" + currentHostIP + "-------------------------------");
+            if (!currentHostIP.equals(recordsValue)) {
+                // 修改解析记录
+                UpdateDomainRecordRequest updateDomainRecordRequest = new UpdateDomainRecordRequest();
+                // 主机记录
+                updateDomainRecordRequest.setRR("dynamicdns");
+                // 记录ID
+                updateDomainRecordRequest.setRecordId(recordId);
+                // 将主机记录值改为当前主机IP
+                updateDomainRecordRequest.setValue(currentHostIP);
+                // 解析记录类型
+                updateDomainRecordRequest.setType("A");
+                UpdateDomainRecordResponse updateDomainRecordResponse = ddns
+                        .updateDomainRecord(updateDomainRecordRequest, client);
+                log_print("updateDomainRecord", updateDomainRecordResponse);
+            }
+        }
+    }
+}
